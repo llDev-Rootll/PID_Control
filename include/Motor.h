0ca053f3b68e21637d1ae3bc0584d6faf6be44bc
Arunava <arunavab713@gmail.com>
2021-10-01 02:00:14 -0400

    Added headed file for class Motor

--- include/Motor.h
+++ include/Motor.h
@@ -1,12 +1,13 @@
 /*Copyright 2021 Arunava Basu, Shon Byron Cortes, Anubhav Paras & Charu Sharma */
 #include <iostream>
-#include <PID.h>
-
+#ifndef PID_H
+#define PID_H
+#endif
 using std::cout;
 using std::cin;
 class Motor{ 
     public:
-        void drive(double target_voltage, double present_voltage);
+        double drive(double target_voltage, double present_voltage);
         double target_velocity;
         double present_velocity;
         double target_voltage;
@@ -14,14 +15,15 @@ class Motor{
         double margin_of_error;
 };
 
-void Motor::drive(double target_velocity, double present_velocity){ 
+double Motor::drive(double target_velocity, double present_velocity){ 
     /*******************************************************************************
  * A function which maps the velocity to the motor voltage and calls the compute function
  * Velocity = L * Voltage where L is a constant of 10
  * The compute function of the PID class is called until the output is within a certain margin of error
+ * Returns the final velocity
  ******************************************************************************/
 ;
-
+return 888;
 }
 
     
