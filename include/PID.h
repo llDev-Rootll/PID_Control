168143abd3464e929900024219813d1cfd6c4693
Charu <charu107@umd.edu>
2021-10-03 16:25:15 -0400

    Added Doxygen for calculate

--- include/PID.h
+++ include/PID.h
@@ -17,6 +17,21 @@ class PID {
    */
   PID(double Kp, double Ki, double Kd);
 
+  /**
+ * @brief A function which computes the PID controller output value. target_voltage is used to store the setpoint
+ * present_voltage is used to store the present voltage value
+ * Steps to calculate output :
+ * 1) error is the difference between the target and the present voltage
+ * 2) The proportional term is Kp times the error
+ * 3) The error is multiplied with the time step dt and added to the integral variable
+ * 4) The integral term is Ki times the integral variable
+ * 5) The derivate term is Kd times the difference in present error and previous error divided by the time step
+ * 6) Total output is the bounded (withing min and max) sum of the proportional, integral, and derivate term 
+ * 
+ * @param target_velocity Desired final velocity
+ * @param present_velocity Current velocity
+ * @return double Final velocity calculated by PID controller
+ */
   double calculate(double target_velocity, double present_velocity);
 
  private:
